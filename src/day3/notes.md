d3893486ab40e31cb031aa269a545cb9e147733a
Luis <luis.geniole@gmail.com>
2022-12-04 20:12:43 -0300

    Finish day 3 part 2.

--- src/day3/notes.md
+++ src/day3/notes.md
@@ -1,6 +1,6 @@
 # Notes
 
-https://adventofcode.com/2022/day/x
+https://adventofcode.com/2022/day/3
 
 ## Part 1
 
@@ -8,5 +8,10 @@ https://adventofcode.com/2022/day/x
 - https://stackoverflow.com/questions/26643688/how-do-i-split-a-string-in-rust
 - https://stackoverflow.com/questions/52882267/how-to-find-if-two-strings-have-common-characters-in-rust
 - https://stackoverflow.com/questions/43983414/how-to-convert-a-rust-char-to-an-integer-so-that-1-becomes-1
+- https://stackoverflow.com/questions/41666235/how-do-i-make-an-rust-item-public-within-a-crate-but-private-outside-it
+- https://doc.rust-lang.org/book/ch19-04-advanced-types.html
+- https://stackoverflow.com/questions/47872494/does-rust-have-an-equivalent-of-cs-typedef
 
 ## Part 2
+
+- https://stackoverflow.com/questions/27904864/what-does-cannot-move-out-of-index-of-mean
--- src/day3/part2.rs
+++ src/day3/part2.rs
@@ -6,11 +6,54 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 20:56:59 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/12/04 11:25:22 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/12/04 20:12:33 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+use super::get_input_lines;
+
+fn find_common_type(group: &(&String, &String, &String)) -> char {
+    let (first, second, third) = group;
+
+    for c in first.chars() {
+        if second.contains(c) && third.contains(c) {
+            return c;
+        }
+    }
+
+    return ' ';
+}
+
+fn resolve_priority(common_type: char) -> u32 {
+    if common_type.is_ascii_lowercase() {
+        return common_type as u32 - 'a' as u32 + 1;
+    }
+
+    if common_type.is_ascii_uppercase() {
+        return common_type as u32 - 'A' as u32 + 27;
+    }
+
+    return 0;
+}
 
 pub fn call() {
-    println!("Part 2: {}.", 0);
+    let mut line_count = 1;
+    let mut total_priorities = 0;
+    let mut rucksacks: Vec<String> = Vec::new();
+
+    for result in get_input_lines() {
+        let rucksack = result.unwrap();
+
+        rucksacks.push(rucksack);
+        if line_count % 3 == 0 {
+            let ref group = (&rucksacks[0], &rucksacks[1], &rucksacks[2]);
+            let common_type = find_common_type(group);
+            total_priorities += resolve_priority(common_type);
+            rucksacks.clear();
+        }
+
+        line_count += 1;
+    }
+
+    println!("Part 1: {} priorities.", total_priorities);
 }
