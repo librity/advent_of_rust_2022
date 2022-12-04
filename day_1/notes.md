d9fceade10caef57c83fc12cc36659531023d677
Luis <luis.geniole@gmail.com>
2022-12-03 21:44:03 -0300

    Redfactor part 1.

--- day_1/notes.md
+++ day_1/notes.md
@@ -9,5 +9,8 @@
 - https://turreta.com/2019/09/07/rust-how-to-compare-strings/
 - https://linuxhint.com/rust-string-to-int/
 - https://stackoverflow.com/questions/27043268/convert-a-string-to-int
+- https://doc.rust-lang.org/std/result/enum.Result.html
+- https://doc.rust-lang.org/book/ch09-02-recoverable-errors-with-result.html
+- https://doc.rust-lang.org/book/ch06-02-match.html
 
 ## Second Star
--- day_1/part_1.rs
+++ day_1/part_1.rs
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.rs                                            :+:      :+:    :+:   */
+/*   part_1.rs                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 20:56:59 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/12/03 21:18:14 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/12/03 21:43:59 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// rustc main.rs -o a.out && ./a.out
+// rustc part_1.rs -o a.out && ./a.out
 
 use std::io::BufRead;
 
@@ -70,12 +70,46 @@ fn print_max_calories() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn result_to_line(option: Option<String>) -> String {
+    match option {
+        None => "0".to_string(),
+        Some(string) => string,
+    }
+}
+
+fn print_max_calories_v2() -> std::io::Result<()> {
+    let file = std::fs::File::open(INPUT_FILE_PATH)?;
+    let reader = std::io::BufReader::new(file);
+    let mut elf_calories = 0;
+    let mut max_calories = 0;
+
+    for result in reader.lines() {
+        let line = result_to_line(result.ok());
+
+        if line.eq("") {
+            if elf_calories > max_calories {
+                max_calories = elf_calories;
+            }
+            elf_calories = 0;
+            continue;
+        }
+
+        let current: i32 = line.parse().unwrap();
+        elf_calories += current;
+    }
+
+    println!("max_calories: {}", max_calories);
+    Ok(())
+}
+
 fn main() {
     // file_string_demo();
     // print_file_line_by_line(INPUT_FILE_PATH);
 
-    match print_max_calories() {
-        Ok(_) => (),
-        Err(error) => println!("Error: {}", error),
-    }
+    // match print_max_calories() {
+    //     Ok(_) => (),
+    //     Err(error) => println!("Error: {}", error),
+    // }
+
+    print_max_calories_v2();
 }
